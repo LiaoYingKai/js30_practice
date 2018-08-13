bf522538d4e1ec4a6e11d5a9890a239234977af0
Kai <b10223052@yuntech.org.tw>
2018-08-14 01:13:02 +0800

    Day22   <p>Esse quibusdam, ad, ducimus cupiditate <a href=>nulla</a>, quae magni odit <a href=>totam</a> ut consequatur eveniet sunt quam provident sapiente dicta neque quod.</p>

---
+++ Day22/javascript.js
@@ -0,0 +1,17 @@
+let trigger = document.querySelectorAll('a');
+let highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+
+function highlineLink() {
+  let link = this.getBoundingClientRect();
+  console.log(link);
+  highlight.style.height = `${link.height}px`;
+  highlight.style.width = `${link.width}px`;
+  highlight.style.transform = `translate(${link.left+window.scrollX}px,${link.top+window.scrollY}px)`;
+  // highlight.style.transform = `translate(${link.left}px,${link.top}px)`;
+
+
+}
+
+trigger.forEach(item => item.addEventListener("mouseenter", highlineLink));
