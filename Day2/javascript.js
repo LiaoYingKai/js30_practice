484688f500e770d76dc66920909853baa445b965
Kai <b10223052@yuntech.org.tw>
2018-08-16 11:25:56 +0800

    fix Day2 click bug

--- Day2/javascript.js
+++ Day2/javascript.js
@@ -1,21 +1,28 @@
-// console.log(hour + " " + minute + " " + second);
 var move_deg_h = document.querySelector(".hour_style");
 var move_deg_m = document.querySelector(".minute_style");
 var move_deg_s = document.querySelector(".second_style");
-
+let date = new Date();
+let second = date.getSeconds();
+console.log(second);
+let second_move = (second / 60) * 360;
+let minute = date.getMinutes();
+console.log(minute);
+let minute_move = (minute / 60) * 360;
+let hour = date.getHours();
+console.log(hour);
+let hour_move = (hour / 12) * 360;
 
 function move() {
-  let date = new Date();
-  let second = date.getSeconds();
-  let second_move = (second / 60) * 360;
+  second_move += 6;
   move_deg_s.style.transform = `rotate(${second_move+90}deg)`;
-  let minute = date.getMinutes();
-  let minute_move = (minute / 60) * 360;
+  if (second_move % 360 == 0) {
+    minute_move += 6;
+  }
   move_deg_m.style.transform = `rotate(${minute_move+90}deg)`;
-  let hour = date.getHours();
-  let hour_move = (hour / 12) * 360;
+  if (minute_move % 360 == 0) {
+    hour_move += 30;
+  }
   move_deg_h.style.transform = `rotate(${hour_move+90}deg)`;
-  // console.log(date);
 }
 
 setInterval(move, 1000);
